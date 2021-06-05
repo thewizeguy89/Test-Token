a9651228c7653336e6c9dcbd87dbb357c1cc5816
thewizeguy89 <56734892+thewizeguy89@users.noreply.github.com>
2021-06-04 20:14:56 -0600

    first draft

---
+++ Contracts/CVTToken.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.5.0;
+
+Contract CVTToken{
+	string public name = "CVTToken";
+	string public symbol = "CVTT";
+	string public standard = "CVTToken v1.0";
+	uint8 public decimals = 18 // same value as wei
+	uint256 public totalsupply;
+
+	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
+
+	event Transfer(
+	address indexed _from
+	address indexed _to
+	uint256 _value
+	);
+
+	event Approval(
+	address indexed _owner
+	address indexed _spender
+	uint256 _value
+	);
+
+	function CVTToken (uint256 _initialSupply) public {
+	balanceof[msg.sender] = _initialSupply;
+	totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        Transfer(_from, _to, _value);
+
+        return true;
+    }
+}
